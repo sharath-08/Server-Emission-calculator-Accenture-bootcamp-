7631bd2673ae6a46dabadc71d296b4c08499f99e
MitchDH <51785406+MitchDH@users.noreply.github.com>
2022-09-29 14:03:18 +0930

    Renaming and formatting
    Renaming and formatting

--- Website/scripts/fetch.js
+++ Website/scripts/fetch.js
@@ -1,6 +1,31 @@
-console.log("cookie")
-
-var obj;
-
-fetch('https://reqres.in/api/users/').then(res => { return res.json() }).then(data => obj = data).then(() => console.log(obj))
-    .then(data => console.log(data)).catch(error => console.log("ERROR"));
+const ul = document.getElementById('authors');
+      const list = document.createDocumentFragment();
+      const url = 'https://jsonplaceholder.typicode.com/users';
+    
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let authors = data;
+          console.log(authors)
+    
+          authors.map(function(author) {
+            let li = document.createElement('li');
+            let name = document.createElement('h2');
+            let email = document.createElement('span');
+    
+            name.innerHTML = `${author.name}`;
+            email.innerHTML = `${author.email}`;
+    
+            li.appendChild(name);
+            li.appendChild(email);
+            list.appendChild(li);
+          })
+        })
+        .then(() => {
+            ul.appendChild(list)
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
--- Website/scripts/my-script.js
+++ Website/scripts/my-script.js
@@ -1,23 +1,3 @@
-var questNum = 0;
-var correct = 0;
-
-function getQuestion() {
-    const question = document.getElementById('cheese');
-    question.innerText = questions[0]["question"];
-
-    const choiceA = document.getElementById('first');
-    choiceA.innerText = questions[questNum]["choiceA"];
-
-    const choiceB = document.getElementById('second');
-    choiceB.innerText = questions[questNum]["choiceB"];
-
-    const choiceC = document.getElementById('third');
-    choiceC.innerText = questions[questNum]["choiceC"];
-
-    const questionNum = document.getElementById('n');
-    questionNum.innerText = "Question " + (questNum+1);
-}
-
 function init() {
     const biscuit = document.getElementById('changeme');
     biscuit.innerText = getUrlParam("age");
@@ -25,46 +5,23 @@ function init() {
     document.getElementById("move").addEventListener("click", next);
 }
 
-function next(event) {
-    var y = getSelection("choices");
-    var z = questions[questNum]["answer"];
-    if (y == z) {
-        correct = correct + 1;
-    }
-    
-    questNum = questNum + 1;
+function getSelection(name)
+{
+    const elem = document.querySelector(`input[name="${name}"]:checked`);
+    return elem ? elem.value : "";
+}
 
-    if (questions.length > questNum) {
-        getQuestion();
-        clearSelection("choices");
-    }
+function clearSelection(name)
+{
+    const elem = document.querySelector(`input[name="${name}"]:checked`);
+    if (elem)
+        elem.checked = false;
+}
 
-    if (questNum == 3) {
-        var x = document.getElementById("move");
-        x.style.display = "none";
-        
-        var a = document.getElementById("results");
-        a.style.display = "block";
-
-        var b = document.getElementById("quiz");
-        b.style.display = "none";
-
-        const finalres = document.getElementById('finalresults');
-        if (correct == 0) {
-            finalres.innerText = "Bad luck. Your final score was 0.0% (0/3).";
-        }
-        else if (correct == 1) {
-            finalres.innerText = "Bad luck. Your final score was 33.3% (1/3).";
-        }
-        else if (correct == 2) {
-            finalres.innerText = "Not bad. Your final score was 66.7% (2/3).";
-        }
-        else {
-            finalres.innerText = "Impressive. Your final score was 100.0% (3/3).";
-        }
-        
-
-    }
+function getUrlParam(name)
+{
+    const params = new URLSearchParams(window.location.search);
+    return params.has(name) ? params.get(name) : "";
 }
 
 init();
