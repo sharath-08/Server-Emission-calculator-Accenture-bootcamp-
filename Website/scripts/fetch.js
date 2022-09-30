0ec2ee125b677a181c4ac197149822be5fe8b777
MitchDH <51785406+MitchDH@users.noreply.github.com>
2022-09-30 09:59:26 +0930

    Updated fetch

--- Website/scripts/fetch.js
+++ Website/scripts/fetch.js
@@ -6,7 +6,7 @@ async function post() {
   const ul = document.getElementById('authors');
   const list = document.createDocumentFragment();
   // const url = 'https://jsonplaceholder.typicode.com/users';
-  const url = 'https://cu2yx38yt8.execute-api.ap-southeast-2.amazonaws.com/Prod/simulation';
+  const url = 'https://fmpucmr0ll.execute-api.ap-southeast-2.amazonaws.com/Prod/simulation';
 
   await fetch(url, {
     method: 'POST',
@@ -29,22 +29,25 @@ async function post() {
         let difference = document.createElement('p');
         let emissions = document.createElement('p');
         let power = document.createElement('p');
+        let percent = document.createElement('b');
 
-        aws_cost.innerHTML = `${"AWS Cost: " + author.aws_cost}`;
-        aws_emission.innerHTML = `${"AWS Emission: " + author.aws_emission}`;
-        aws_power.innerHTML = `${"AWS Power: " + author.aws_power}`;
-        cost.innerHTML = `${"Local Cost: " + author.cost}`;
-        difference.innerHTML = `${"Difference: " + author.difference}`;
-        emissions.innerHTML = `${"Local Emissions: " + author.emissions}`;
-        power.innerHTML = `${"Local Power: " + author.power}`;
+        aws_cost.innerHTML = `${"Cloud Cost of Energy Consumprtion: " + author.aws_cost + " cents"}`;
+        aws_emission.innerHTML = `${"Cloud Co2 Emissions: " + author.aws_emission + " grams"}`;
+        aws_power.innerHTML = `${"Cloud Power Consumption: " + author.aws_power + " killowats"}`;
+        cost.innerHTML = `${"Local Cost of Energy Consumption: " + author.cost + " cents"}`;
+        difference.innerHTML = `${"Emissions Difference (Local - Cloud): " + author.difference + " grams"}`;
+        emissions.innerHTML = `${"Local Co2 Emissions: " + author.emissions + " grams"}`;
+        power.innerHTML = `${"Local Power Consumption: " + author.power + " killowatts"}`;
+        percent.innerHTML = `${"There is an approximate " + (author.aws_emission/author.emissions)*100 + "% reduction in Co2 emissions when swtiching to a cloud provider!"}`;
 
-        li.appendChild(aws_cost);
-        li.appendChild(aws_emission);
+        li.appendChild(power);
         li.appendChild(aws_power);
         li.appendChild(cost);
-        li.appendChild(difference);
+        li.appendChild(aws_cost);
         li.appendChild(emissions);
-        li.appendChild(power);
+        li.appendChild(aws_emission);
+        li.appendChild(difference);
+        li.appendChild(percent);
         list.appendChild(li);
       })
     })
